2bf7f16d32ec4f72dbf68569092dcd7f2258c551
AlekseyK <aleksey.kabishau@gmail.com>
2020-05-08 22:29:58 -0400

    feat: add assert with some parameter check

--- ConcentrationGame/Model/Concetration.swift
+++ ConcentrationGame/Model/Concetration.swift
@@ -15,17 +15,18 @@ class Concentration {
 	
 	
 	init(numberOfPairsOfCard: Int) {
+		assert(numberOfPairsOfCard > 0, "Concentration.init - numberOfPairsOfCard is \(numberOfPairsOfCard) but should be more then 0")
 		for _ in 0..<numberOfPairsOfCard {
 			let card = Card()
 			cards += [card, card]
 		}
-		// TODO: Shuffle the card
 		cards.shuffle()
 	}
 	
 	
 	func chooseCard(at index: Int) {
 		if !cards[index].isMatched {
+			assert(cards.indices.contains(index), "Concentration.chooseCard - Index \(index) is not in cards.indices ")
 			if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
 				if cards[matchIndex].id == cards[index].id {
 					cards[matchIndex].isMatched = true
