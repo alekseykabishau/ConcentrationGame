5be441c984ff26e354d6cc1334b66ee277a2bdf9
AlekseyK <aleksey.kabishau@gmail.com>
2020-05-09 14:24:08 -0400

    refactor: improve readability of indexOfOneAndOnlyFaceUpCard using extension and filter function

--- ConcentrationGame/Model/Concetration.swift
+++ ConcentrationGame/Model/Concetration.swift
@@ -14,17 +14,7 @@ class Concentration {
 
 	private var indexOfOneAndOnlyFaceUpCard: Int? {
 		get {
-			var foundIndex: Int?
-			for index in cards.indices {
-				if cards[index].isFaceUp {
-					if foundIndex == nil {
-						foundIndex = index
-					} else {
-						return nil
-					}
-				}
-			}
-			return foundIndex
+			return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
 		}
 		set {
 			cards.indices.forEach { index in
@@ -57,5 +47,11 @@ class Concentration {
 				indexOfOneAndOnlyFaceUpCard = index
 			}
 		}
+	}
+}
+
+extension Collection {
+	var oneAndOnly: Element? {
+		return count == 1 ? first : nil
 	}
 }
